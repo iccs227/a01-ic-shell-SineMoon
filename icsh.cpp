e0f0a27cd10bfb0341188558d4d21e0cf0f7fb78
SineMoon <sinecherlyn@gmail.com>
2025-04-29 15:11:43 +0700

    fix to C++ tag: 0.2.0

--- icsh.cpp
+++ icsh.cpp
@@ -1,13 +1,14 @@
 /* ICCS227: Project 1: icsh
  * Name: Cherlyn Wijitthadarat
  * StudentID: 6480330
- * Tag: 0.1.0
+ * Tag: 0.2.0
  */
 
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <string>
-#include <sstream>
+#include <fstream>
 
 using namespace std;
 
@@ -21,58 +22,85 @@ vector<string> toTokens(const string& buffer) {
     return tokens;
 }
 
-void printTokens(const vector<string>& tokens, int start) {
+void printToken(const vector<string>& tokens, size_t start) {
     for (size_t i = start; i < tokens.size(); ++i) {
         cout << tokens[i] << " ";
     }
     cout << endl;
 }
 
-void command(const vector<string>& curr, const vector<string>& prev) {
+void command(vector<string>& curr, vector<string>& prev) {
     if (curr.empty()) return;
 
     if (curr[0] == "!!" && curr.size() == 1) {
         if (!prev.empty()) {
-            for (const auto& tok : prev) {
-                cout << tok << " ";
-            }
-            cout << endl;
-	    command(prev, prev);
+            command(prev, prev);
         } else {
             cout << "No previous output" << endl;
         }
-    } else if (curr[0] == "echo" && curr.size() > 1) {
-        printTokens(curr, 1);
-    } else if (curr[0] == "exit" && curr.size() == 1) {
-        cout << "bye" << endl;
-        exit(0);
-    } else if (curr[0] == "exit" && curr.size() == 2) {
-        cout << "bye" << endl;
-        int n = stoi(curr[1]);
-        if (n > 255) {
-            n = n & 0xFF;
-        }
-        exit(n);
+
     } else {
-        cout << "bad command" << endl;
+        if (curr[0] == "echo" && curr.size() > 1) {
+            printToken(curr, 1);
+        } else if (curr[0] == "exit" && curr.size() == 2) {
+            cout << "$ echo $?\n";
+            int e = stoi(curr[1]);
+            if (e > 255) {
+                e = e & 0xFF;
+            }
+            cout << e << endl;
+            exit(e);
+        } else if (curr[0] == "exit" && curr.size() == 1) {
+            cout << "$ echo $?\n0\n$" << endl;
+            exit(0);
+        } else {
+            cout << "bad command" << endl;
+            exit(1);
+        }
+        prev = curr;
     }
 }
 
-int main() {
-    cout << "Starting IC shell" << endl;
 
+void readScript(const string& fileName) {
+    ifstream file(fileName);
+    if (!file.is_open()) {
+        return;
+    }
+
+    string buffer;
     vector<string> prevBuffer;
-    string input;
 
-    while (true) {
-        cout << "icsh $ ";
-        getline(cin, input);
-        if (input.empty()) continue;
+    while (getline(file, buffer)) {
+        if (buffer.empty()) continue;
 
-        vector<string> currBuffer = toTokens(input);
+        vector<string> currBuffer = toTokens(buffer);
         command(currBuffer, prevBuffer);
+
         prevBuffer = currBuffer;
     }
+}
 
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        readScript(argv[1]);
+    } else {
+        cout << "Starting IC shell" << endl;
+        string buffer;
+        vector<string> prevBuffer;
+
+        while (true) {
+            cout << "icsh $ ";
+            getline(cin, buffer);
+
+            if (buffer.empty()) continue;
+
+            vector<string> currBuffer = toTokens(buffer);
+            command(currBuffer, prevBuffer);
+
+            prevBuffer = currBuffer;
+        }
+    }
     return 0;
 }
+
